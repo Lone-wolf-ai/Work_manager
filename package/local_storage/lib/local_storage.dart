fb3ca6fcb82282a90ddf8733d47c17dc3e8361a7
Syed Tanjim Ahmed <tanjimahmed327@gmail.com>
2024-11-05 11:56:07 +0600

    realase_package

--- package/local_storage/lib/local_storage.dart
+++ package/local_storage/lib/local_storage.dart
@@ -1,4 +1,5 @@
-import 'package:get_storage/get_storage.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class LocalStorage {
   static final LocalStorage _instance = LocalStorage._internal();
@@ -9,27 +10,31 @@ class LocalStorage {
 
   LocalStorage._internal();
 
-  final _storage = GetStorage();
+  late Box _storage;
+
+  // Initialize Hive
+  Future<void> init() async {
+    await Hive.initFlutter();
+    _storage = await Hive.openBox('localStorage');
+  }
 
   // Generic method to save data
   Future<void> saveData<T>(String key, T value) async {
-    await _storage.write(key, value);
+    await _storage.put(key, value);
   }
 
   // Generic method to read data
   T? readData<T>(String key) {
-    return _storage.read<T>(key);
+    return _storage.get(key);
   }
 
   // Generic method to remove data
   Future<void> removeData(String key) async {
-    await _storage.remove(key);
+    await _storage.delete(key);
   }
 
   // Clear all data in storage
   Future<void> clearAll() async {
-    await _storage.erase();
+    await _storage.clear();
   }
 }
-
-
